333b99d37cfb4269b77b97b7820b2b5cad6a876f
lantianlaoli <84974164+cxp-13@users.noreply.github.com>
2024-01-11 17:55:30 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/8k908hllK_5nCAqNqqH1W6kZGxY_FpBG`,
-      accounts: [`641943fa6c3fab18fed274c2b3194f0d71383ecfb9a58b2d70188e693c245510`]
+      url: `YOUR_ALCHEMY_PROVIDER_APIA`,
+      accounts: [`YOUR_PRIVATE_KEY`]
     },
   }
 };
